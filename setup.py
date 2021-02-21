e441cfb3d05da3f821fc5edd08187f3e70f01724
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-02-21 17:32:35 -0500

    oops. forgot the setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup file to describe my project."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fun",
+    version="0.1",
+    description="Week 5 Homework 2 repository",
+    author="Chris Fauerbach",
+    author_email="chfauerbach@vcu.edu",
+    packages=find_packages(),
+)
