936b85ba9082b39f1cda5d04c473cdc569d7ff36
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-03-02 17:36:44 -0500

    updating test file to pass and validate information

--- fun/homework.py
+++ fun/homework.py
@@ -1,31 +1,34 @@
 """Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
-    """ 
-        Required parameter, incoming_list, should be a list.
-        Find the largest number in the list. 
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the largest number in the list.
     """
     pass
+
 
 def find_least_number(incoming_list):
-    """ 
-        Required parameter, incoming_list, should be a list.
-        Find the smallest/least number in the list. 
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the smallest/least number in the list.
     """
     pass
+
 
 def add_list_numbers(incoming_list):
-    """ 
-        Required parameter, incoming_list, should be a list.
-        Add all the values together and return it.
+    """
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
     """
 
     pass
 
-def get_key_with_longest_value(incoming_dict):
+
+def longest_value_key(incoming_dict):
     """
-        Required parameter, incoming_dict, should be a dict.
-        Find the KEY that has a value with the highest length, use the len() function
+    Required parameter, incoming_dict, should be a dict.
+    Find the KEY that has a value with the highest length, use the len() function
     """
     pass
-
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,17 +5,29 @@ from fun import homework
 
 
 def test_greatest_number():
+    """Quick test to see if you can find the largest number in a list."""
     assert homework.find_greatest_number([1, 2, 3, 4, 5, 6, 7, 8]) == 8
     assert homework.find_greatest_number([7, 3, 21, 4, 1, 6]) == 21
 
 
 def test_least_number():
+    """Quick test to see if you can find the smallest number in a list."""
     assert homework.find_least_number([1, 2, 3, 4, 5, 6, 7, 8]) == 1
     assert homework.find_least_number([8, 7, 3, 2, 4, 1, 6]) == 1
 
 
 def test_sum_of_list():
+    """Quick test to see if you can add together all the numbers in a list."""
     assert homework.add_list_numbers([1, 2, 3, 4]) == 10
+    assert homework.add_list_numbers([]) == 0
+    assert homework.add_list_numbers(None) == 0
+
 
 def test_key_with_the_longest_value():
-    assert homework.get_key_with_longest_value("dog": "cat", "a": "asdfasdfasdfasdfasdf") == "a"
+    """A little trickier.   Which KEY has the 'longest' value associated with it? """
+
+    assert (
+        homework.longest_value_key({"dog": "cat", "a": "asdfasdfasdfasdfasdf"}) == "a"
+    )
+    assert homework.longest_value_key({}) is None
+    assert homework.longest_value_key(None) is None
