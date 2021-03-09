3b0e62db445ed2ec9152ad414e994018385a6954
vcueasterlingkh <78060887+vcueasterlingkh@users.noreply.github.com>
2021-03-08 21:33:29 -0500

    try 4

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -18,9 +18,9 @@ def test_least_number():
 
 def test_sum_of_list():
     """Quick test to see if you can add together all the numbers in a list."""
-    homework.add_list_numbers([1, 2, 3, 4]) == 10
-    homework.add_list_numbers([]) == 0
-    homework.add_list_numbers(None) == 0
+    assert homework.add_list_numbers([1, 2, 3, 4]) == 10
+    assert homework.add_list_numbers([]) == 0
+    assert homework.add_list_numbers(None) == 0
 
 
 def test_key_with_the_longest_value():
